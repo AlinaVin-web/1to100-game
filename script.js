3b77b5df1f648a2e1a157e8e99f880f284b41e76
Alina Vinogradova <ellafoss@yandex.ru>
2024-09-29 00:14:30 +0300

    Подчищено лишнее, стилизация кода

--- script.js
+++ script.js
@@ -9,29 +9,26 @@ function stringToNumber(str) {
 }
 
 function game(rNum) {
-    let result;
-
     function gameAttempt(text) {
+
         let gamerNum = prompt(text, '');
+
         if (gamerNum === null) {
             alert("Игра окончена");
-            result = "game over";
             return;
         }
+
         gamerNum = stringToNumber(gamerNum);
+
         if (gamerNum) {
             if (gamerNum > rNum) gameAttempt("Загаданное число меньше. Введите другое число");
             else if (gamerNum < rNum) gameAttempt("Загаданное число больше. Введите другое число");
-            else if (gamerNum == rNum) {
-                alert("Поздравляю, Вы угадали!!!");
-                result = "win";
-            }
+            else if (gamerNum == rNum) alert("Поздравляю, Вы угадали!!!");
         }
         else gameAttempt("Введите число!");
     }
 
-    gameAttempt("Введите число от 1 до 100");
-    return result;
+    return gameAttempt("Введите число от 1 до 100");
 }
 
 const randNum = Math.ceil(Math.random() * 100);
